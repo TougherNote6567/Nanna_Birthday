b39ed1e7ff108d8dc5cedc6a57fe1936fe86b7ed
TougherNote6567 <80233752+TougherNote6567@users.noreply.github.com>
2021-07-15 16:53:50 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+var canvas = new fabric.Canvas("bdayCanvas");
+ var song= document.getElementById("myAudio");
+var image_object="";
+
+function canvas_background(){
+    fabric.Image.fromURL('BirthdayImage.jpg', function (Img){
+image_object=Img;
+
+image_object.scaleToWidth(700);
+image_object.scaleToHeight(510);
+image_object.set({
+    top:0,
+    left:0
+});
+canvas.add(image_object);
+});
+}
+function playSound(){
+	song.play();
+}
